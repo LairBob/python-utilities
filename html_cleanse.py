fbd3313e2ef9ef4683c520a88939727c3b183d4e
Laurent Stanevich <laurent@stanevich.com>
2016-09-01 10:33:31 -0400

    Minor cleanups

--- html_cleanse.py
+++ html_cleanse.py
@@ -12,8 +12,6 @@ from urllib.parse import urlparse, urlunparse
 #%% Page-cleansing regexprs
 
 regDivStrip = re.compile('^([\s\S]*)(\<div class="inner-html" id="body-content">)([\s\S]*)(\<!-- inner-html -->[\s]*\<\/div\>)([\s\S]*)$')
-regAnchBold = re.compile('(\<a .*\>)[\s\S]*\<strong>([\s\S]*)\<\/strong>[\s\S]*(\<\/a[\s]*>)')
-regH2Bold = re.compile('(\<h2[\s\S]*\>)[\s\S]*\<strong>([\s\S]*)\<\/strong>[\s\S]*(\<\/h2[\s\S]*>)')
 
 
 #%% Regular expressions to compile
@@ -52,44 +50,9 @@ def cleanseHTMLSoup(soupInnerHTML):
     for eachSpan in soupInnerHTML.select("span"):
         eachSpan = eachSpan.unwrap()
 
-#    for eachDirAttr in soupInnerHTML.find_all(dir=True):
-#        del eachDirAttr['dir']
-#
-#    for eachAttr in soupInnerHTML.find_all(width=True):
-#        del eachAttr['width']
-#
-#    for eachAttr in soupInnerHTML.find_all(height=True):
-#        del eachAttr['height']
-#
-#    for eachAttr in soupInnerHTML.find_all(valign=True):
-#        del eachAttr['valign']
-#
-#    for eachAttr in soupInnerHTML.find_all(cellpadding=True):
-#        del eachAttr['cellpadding']
-#
-#    for eachAttr in soupInnerHTML.find_all(cellspacing=True):
-#        del eachAttr['cellspacing']
-#
-#    for eachAttr in soupInnerHTML.find_all(border=True):
-#        del eachAttr['border']
-#
-#    for eachAttr in soupInnerHTML.find_all(align=True):
-#        del eachAttr['align']
-#
-#    for eachAttr in soupInnerHTML.find_all(type=True):
-#        del eachAttr['type']
-
     for eachTag in soupInnerHTML():
         for eachAttribute in ["class", "id", "name", "style", "dir", "width", "height", "valign", "cellpadding", "cellspacing", "border", "align", "type"]:
             del eachTag[eachAttribute]
-
-    # attrList = ['width', 'height', 'valign', 'cellpadding', 'cellspacing', 'border', 'align']
-    # for eachAttribute in attrList:
-    #     for eachAttr in soupInnerHTML.find_all(eachAttribute=True):
-    #         del eachAttr[eachAttr]
-
-    #    for eachClassAttr in soupPage.find_all(hasClass):
-    #        del eachClassAttr['class']
 
     return soupInnerHTML
 
@@ -99,8 +62,6 @@ def cleanseHTMLStr(strSourceHTML):
     strCleansedHTML = strSourceHTML
 
     strCleansedHTML = regDivStrip.search(strCleansedHTML).group(1)+regDivStrip.search(strCleansedHTML).group(3)+regDivStrip.search(strCleansedHTML).group(5)
-    # strCleansedHTML = regAnchBold.sub('\g<1>\g<2>\g<3>', strCleansedHTML)
-    # strCleansedHTML = regH2Bold.sub('\g<1>\g<2>\g<3>', strCleansedHTML)
     strCleansedHTML = strCleansedHTML.replace('<br/>', '')
     strCleansedHTML = regPEmpty.sub('', strCleansedHTML)
     strCleansedHTML = regPComment.sub('\g<2>', strCleansedHTML)
