204bdcb1125c824647dac3794d8f87bd3b968783
Laurent Stanevich (OS X Windows) <laurent@stanevich.com>
2016-08-31 20:03:22 -0400

    Interim commit

--- util_logging.py
+++ util_logging.py
@@ -1,6 +1,23 @@
 
+import logging
 
-def logtest():
-	print('Logging Module Test')
 
-	return
+#%% Logging setup modules
+
+def logSetup():
+    myLogger = logging.getLogger("log_main")
+    myLogger.setLevel(logging.INFO)
+    
+ 
+    # create the logging file handler
+    fh = logging.FileHandler("log_main.log", mode="w")
+ 
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+ 
+    # add handler to logger object
+    myLogger.addHandler(fh)
+    return myLogger
+
+ 
+myLogger = logSetup()
