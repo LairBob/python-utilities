1050083fafc60ef4a0471d8817c8ac1221150a69
Laurent Stanevich (OS X Windows) <laurent@stanevich.com>
2016-09-02 18:37:29 -0400

    Properties now managed within instance-level dict
    This allows each logger instance to maintain its own unique set of
    properties

--- util_logging.py
+++ util_logging.py
@@ -5,39 +5,50 @@ Created on Thu Sep  1 22:53:00 2016
 @author: lstanevich
 """
 
+import os
+
 class clsLogger:
     
-    _logPath = "\\logs"
-    _logName = "log-out"
-    _logExt = ".log"
-    
     def __init__( self, **logArgs ):
-        if "logPath" in logArgs:
-            self.logPath ( logArgs[ 'logPath' ] )
-        if "logName" in logArgs:
-            self.logName ( logArgs[ 'logName' ] )
-        if "logExt" in logArgs:
-            self.logPath ( logArgs[ 'logExt' ] )
+        self._properties = {}
+        self._properties[ 'root' ] = os.getcwd()
+        self._properties[ 'logPath' ] = "\\logs"
+        self._properties[ 'logName' ] = "log-out"
+        self._properties[ 'logExt' ] = ".log"
+        
+        for logArg in logArgs:
+            if logArg in self._properties:
+                # Check if the property needs to be passed through its dedicated handler
+                if logArg == "logPath":
+                    self.logPath( logArgs[ logArg ] )
+#                elif logArg == "logName":
+#                    self.logName( logArgs[ logArg ] )
+#                elif logArg == "logExt":
+#                    self.logName( logArgs[ logArg ] )
+                else:                    
+                    self._properties[ logArg ] = logArgs[ logArg ]
+            else:
+                raise Exception(" UKNOWN PROPERTY: "+logArgs[ logArg ])        
  
     def logPath( self, *args ):
         if args:
             tmpPath = args[0]
             if tmpPath[0] != "\\":
                 tmpPath = "\\"+tmpPath
-            if tmpPath[0] != "\\":
+            if tmpPath[-1] == "\\":
                 tmpPath = tmpPath[:-1]
-            self._logPath = tmpPath
-        return self._logPath
+            self._properties[ 'logPath' ] = tmpPath
+        return self._properties[ 'logPath' ]
 
     def logName( self, *args ):
         if args:
-            self._logName = args[0]
-        return self._logName+self.logExt()
+            self._properties[ 'logName' ] = args[0]
+        return self._properties[ 'logName' ]+self.logExt()
 
     def logExt( self, *args ):
         if args:
-            self._logExt = args[0]
-        return self._logExt
+            self._properties[ 'logExt' ] = args[0]
+        return self._properties[ 'logExt' ]
 
 
 
@@ -48,7 +59,10 @@ if __name__ == '__main__':
     # Path tests
     tmpLogger = clsLogger( logPath = "path", logName = "log-main" )
     print ( tmpLogger.logPath() )
-    print ( tmpLogger.logPath( '\\sub' ) )
+    print ( tmpLogger.logPath( '\\subleft' ) )
+    print ( tmpLogger.logPath( '\\subboth\\' ) )
+    print ( tmpLogger.logPath( 'subright\\' ) )
+    print ( tmpLogger.logPath( 'subnone' ) )
     print ( tmpLogger.logPath() )
  
     print ( tmpLogger.logName() )
