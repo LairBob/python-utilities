a3d1e133e84f79cbcb97b9b1c4410a9289e3ceae
Laurent Stanevich (OS X Windows) <laurent@stanevich.com>
2016-09-02 19:32:01 -0400

    Handles filenames that includes extensions

--- util_logging.py
+++ util_logging.py
@@ -10,26 +10,28 @@ import os
 class clsLogger:
     
     def __init__( self, **logArgs ):
+        
+        # Create a per-instance dict to store local properties
         self._properties = {}
         self._properties[ 'root' ] = os.getcwd()
         self._properties[ 'logPath' ] = "\\logs"
         self._properties[ 'logName' ] = "log-out"
         self._properties[ 'logExt' ] = ".log"
         
+        # Traverse through any kw arguments that were passed
         for logArg in logArgs:
+            
+            # If it's a "known" argument, then process it
             if logArg in self._properties:
                 # Check if the property needs to be passed through its dedicated handler
                 if logArg == "logPath":
                     self.logPath( logArgs[ logArg ] )
-#                elif logArg == "logName":
-#                    self.logName( logArgs[ logArg ] )
-#                elif logArg == "logExt":
-#                    self.logName( logArgs[ logArg ] )
                 else:                    
                     self._properties[ logArg ] = logArgs[ logArg ]
             else:
                 raise Exception(" UKNOWN PROPERTY: "+logArgs[ logArg ])        
  
+    # Dedicated method exposed to process path property   
     def logPath( self, *args ):
         if args:
             tmpPath = args[0]
@@ -40,11 +42,18 @@ class clsLogger:
             self._properties[ 'logPath' ] = tmpPath
         return self._properties[ 'logPath' ]
 
+    # Dedicated method exposed to process name property   
     def logName( self, *args ):
         if args:
-            self._properties[ 'logName' ] = args[0]
+            tmpName = args[0]
+            if '.' in tmpName:
+                self._properties[ 'logName' ] = tmpName.split('.')[0]
+                self.logExt( '.'+tmpName.split('.')[1] )
+            else:
+                self._properties[ 'logName' ] = tmpName
         return self._properties[ 'logName' ]+self.logExt()
 
+    # Dedicated method exposed to process extension property   
     def logExt( self, *args ):
         if args:
             self._properties[ 'logExt' ] = args[0]
@@ -67,5 +76,6 @@ if __name__ == '__main__':
  
     print ( tmpLogger.logName() )
     print ( tmpLogger.logName( 'log-test' ) )
+    print ( tmpLogger.logName( 'log-test.txt' ) )
     print ( tmpLogger.logName() )
    
