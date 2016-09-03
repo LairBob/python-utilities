fd5a81b52783e4eda5d55fbefbf95032ce4ee118
Laurent Stanevich (OS X Windows) <laurent@stanevich.com>
2016-09-02 21:19:19 -0400

    Re-organized properties and property names

--- util_logging.py
+++ util_logging.py
@@ -17,7 +17,7 @@ class clsLogger:
         # Create a per-instance dict to store local properties
         self._properties = {}
         self._properties[ 'root' ] = os.getcwd()
-        self._properties[ 'logPath' ] = [ "logs" ]
+        self._properties[ 'relLoc' ] = [ "logs" ]
         self._properties[ 'logName' ] = "log-out"
         self._properties[ 'logExt' ] = ".log"
         
@@ -27,8 +27,8 @@ class clsLogger:
             # If it's a "known" argument, then process it
             if logArg in self._properties:
                 # Check if the property needs to be passed through its dedicated handler
-                if logArg == "logPath":
-                    self.logPath( logArgs[ logArg ] )
+                if logArg == "relLoc":
+                    self.relLoc( logArgs[ logArg ] )
                 else:                    
                     self._properties[ logArg ] = logArgs[ logArg ]
             else:
@@ -37,20 +37,33 @@ class clsLogger:
  
  #%% Property interface functions
  
+    # Dedicated method exposed to process root path property   
+    def root( self, *args ):
+        if args:
+            tmpRoot = args[0]
+#            raise Exception( "Changing Root: "+self._properties[ 'root' ]+" --> "+tmpRoot )
+            self._properties[ 'root' ] = tmpRoot
+        return self._properties[ 'root' ]
+
     # Dedicated method exposed to process path property   
-    def logPath( self, *args ):
+    def relLoc( self, *args ):
         if args:
             tmpPath = args[0]
+#            raise Exception( "Changing Path: "+os.path.join( *self._properties[ 'relLoc' ] )+" --> "+tmpPath )
             for eachSep in [ "\\", "/" ]:
                 if eachSep in tmpPath:
-                    self._properties[ 'logPath' ] = tmpPath.split( eachSep )
+                    self._properties[ 'relLoc' ] = tmpPath.split( eachSep )
                     break
-        return os.path.join( *self._properties[ 'logPath' ] )
+        return os.path.join( *self._properties[ 'relLoc' ] )
+
+    def absLoc( self ):
+        return os.path.join( self.root(), self.relLoc() )
 
     # Dedicated method exposed to process name property   
     def logName( self, *args ):
         if args:
             tmpName = args[0]
+#            raise Exception( "Changing Name: "+self._properties[ 'logName' ]+" --> "+tmpName )
             if '.' in tmpName:
                 self._properties[ 'logName' ] = tmpName.split('.')[0]
                 self.logExt( '.'+tmpName.split('.')[1] )
@@ -62,20 +75,14 @@ class clsLogger:
     def logExt( self, *args ):
         if args:
             tmpExt = args[0]
+#            raise Exception( "Changing Extansion: "+self._properties[ 'logExt' ]+" --> "+tmpExt )
             if not tmpExt[0] == '.':
                 tmpExt = '.'+tmpExt
             self._properties[ 'logExt' ] = tmpExt
         return self._properties[ 'logExt' ]
 
-    # Dedicated method exposed to process root path property   
-    def root( self, *args ):
-        if args:
-            tmpRoot = args[0]
-            self._properties[ 'root' ] = tmpRoot
-        return self._properties[ 'root' ]
-
     def relPath( self ):
-        return os.path.join( self.logPath(), self.logName() )
+        return os.path.join( self.relLoc(), self.logName() )
 
     def absPath( self ):
         return os.path.join( self.root(), self.relPath() )
@@ -86,25 +93,31 @@ class clsLogger:
 if __name__ == '__main__':
     
     # Path tests
-    tmpLogger = clsLogger( logPath = "path", logName = "log-main" )
+    tmpLogger = clsLogger( relLoc = "path", logName = "log-main" )
 
     print ( tmpLogger.root() ) 
+    print ( tmpLogger.relLoc() )
+    print ( tmpLogger.absLoc() )
+
+    print ( tmpLogger.logName() )
+    print ( tmpLogger.logExt() )
+    
     print ( tmpLogger.relPath() )
     print ( tmpLogger.absPath() )
-    
-    print ( tmpLogger.logPath() )
-    print ( tmpLogger.logPath( '\\subleft' ) )
-    print ( tmpLogger.logPath( '\\subboth\\' ) )
-    print ( tmpLogger.logPath( 'subright\\' ) )
-    print ( tmpLogger.logPath( 'subnone' ) )
-    print ( tmpLogger.logPath( 'subpar\\subchild' ) )
-    print ( tmpLogger.absPath() )
  
-    print ( tmpLogger.logName() )
-    print ( tmpLogger.logName( 'log-test' ) )
-    print ( tmpLogger.logName( 'log-test.txt' ) )
-    print ( tmpLogger.logExt( '.csv' ) )
-    print ( tmpLogger.logExt( 'txt' ) )
-    print ( tmpLogger.logName() )
+    
+#%% Stress tests
 
-   
+    testStress = False
+
+    if testStress:
+        print ( tmpLogger.relLoc( '\\subleft' ) )
+        print ( tmpLogger.relLoc( '\\subboth\\' ) )
+        print ( tmpLogger.relLoc( 'subright\\' ) )
+        print ( tmpLogger.relLoc( 'subnone' ) )
+        print ( tmpLogger.relLoc( 'subpar\\subchild' ) )
+        
+        print ( tmpLogger.logName( 'log-test' ) )
+        print ( tmpLogger.logName( 'log-test.txt' ) )
+        print ( tmpLogger.logExt( '.csv' ) )
+        print ( tmpLogger.logExt( 'txt' ) )
