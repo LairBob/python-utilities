23cccc6d6629c951d13a6d2abbc617c45a6f5bb3
Laurent Stanevich (OS X Windows) <laurent@stanevich.com>
2016-09-02 22:07:38 -0400

    Now depends on centralized file manager modules

--- util_logging.py
+++ util_logging.py
@@ -5,87 +5,18 @@ Created on Thu Sep  1 22:53:00 2016
 @author: lstanevich
 """
 
-import os
+from util_file_managers import fManager
+
 
 class clsLogger:
 
-
 #%% Class constructor
     
-    def __init__( self, **logArgs ):
-        
-        # Create a per-instance dict to store local properties
-        self._properties = {}
-        self._properties[ 'root' ] = os.getcwd()
-        self._properties[ 'relLoc' ] = [ "logs" ]
-        self._properties[ 'logName' ] = "log-out"
-        self._properties[ 'logExt' ] = ".log"
-        
-        # Traverse through any kw arguments that were passed
-        for logArg in logArgs:
-            
-            # If it's a "known" argument, then process it
-            if logArg in self._properties:
-                # Check if the property needs to be passed through its dedicated handler
-                if logArg == "relLoc":
-                    self.relLoc( logArgs[ logArg ] )
-                else:                    
-                    self._properties[ logArg ] = logArgs[ logArg ]
-            else:
-                raise Exception(" UKNOWN PROPERTY: "+logArgs[ logArg ])        
- 
- 
- #%% Property interface functions
- 
-    # Dedicated method exposed to process root path property   
-    def root( self, *args ):
-        if args:
-            tmpRoot = args[0]
-#            raise Exception( "Changing Root: "+self._properties[ 'root' ]+" --> "+tmpRoot )
-            self._properties[ 'root' ] = tmpRoot
-        return self._properties[ 'root' ]
+    def __init__( self, **fArgs ):
+        self.f = fManager( **fArgs )
 
-    # Dedicated method exposed to process path property   
-    def relLoc( self, *args ):
-        if args:
-            tmpPath = args[0]
-#            raise Exception( "Changing Path: "+os.path.join( *self._properties[ 'relLoc' ] )+" --> "+tmpPath )
-            for eachSep in [ "\\", "/" ]:
-                if eachSep in tmpPath:
-                    self._properties[ 'relLoc' ] = tmpPath.split( eachSep )
-                    break
-        return os.path.join( *self._properties[ 'relLoc' ] )
+#%% File management functions
 
-    def absLoc( self ):
-        return os.path.join( self.root(), self.relLoc() )
-
-    # Dedicated method exposed to process name property   
-    def logName( self, *args ):
-        if args:
-            tmpName = args[0]
-#            raise Exception( "Changing Name: "+self._properties[ 'logName' ]+" --> "+tmpName )
-            if '.' in tmpName:
-                self._properties[ 'logName' ] = tmpName.split('.')[0]
-                self.logExt( '.'+tmpName.split('.')[1] )
-            else:
-                self._properties[ 'logName' ] = tmpName
-        return self._properties[ 'logName' ]+self.logExt()
-
-    # Dedicated method exposed to process extension property   
-    def logExt( self, *args ):
-        if args:
-            tmpExt = args[0]
-#            raise Exception( "Changing Extansion: "+self._properties[ 'logExt' ]+" --> "+tmpExt )
-            if not tmpExt[0] == '.':
-                tmpExt = '.'+tmpExt
-            self._properties[ 'logExt' ] = tmpExt
-        return self._properties[ 'logExt' ]
-
-    def relPath( self ):
-        return os.path.join( self.relLoc(), self.logName() )
-
-    def absPath( self ):
-        return os.path.join( self.root(), self.relPath() )
 
 
 #%% Unit tests
@@ -93,17 +24,17 @@ class clsLogger:
 if __name__ == '__main__':
     
     # Path tests
-    tmpLogger = clsLogger( relLoc = "path", logName = "log-main" )
+    tmpLogger = clsLogger( relLoc = "path", fileName = "log-main" )
 
-    print ( tmpLogger.root() ) 
-    print ( tmpLogger.relLoc() )
-    print ( tmpLogger.absLoc() )
+    print ( tmpLogger.f.root() ) 
+    print ( tmpLogger.f.relLoc() )
+    print ( tmpLogger.f.absLoc() )
 
-    print ( tmpLogger.logName() )
-    print ( tmpLogger.logExt() )
+    print ( tmpLogger.f.fileName() )
+    print ( tmpLogger.f.fileExt() )
     
-    print ( tmpLogger.relPath() )
-    print ( tmpLogger.absPath() )
+    print ( tmpLogger.f.relPath() )
+    print ( tmpLogger.f.absPath() )
  
     
 #%% Stress tests
