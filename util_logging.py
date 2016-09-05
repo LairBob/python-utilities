46114269f9adff14f9df0fbb052bc8411a0fdb99
Laurent Stanevich (OS X Windows) <laurent@stanevich.com>
2016-09-04 23:29:58 -0400

    Basic Logger object being created correctly

--- util_logging.py
+++ util_logging.py
@@ -6,49 +6,53 @@ Created on Thu Sep  1 22:53:00 2016
 """
 
 from util_file_managers import fManager
-
+from config_main import strTimestamp
 
 class clsLogger:
 
 #%% Class constructor
-    
+
     def __init__( self, **fArgs ):
-        self.f = fManager( **fArgs )
+
+        self._properties = {}
+        self._properties['indent'] = 0
+        self._properties['indentSize'] = 4
+
+        self._fM = fManager( **fArgs )
+
+        self._currTime = strTimestamp
+
+        self.write( "BEGINNING LOG OUTPUT: "+strTimestamp )
+
+        for fArg in fArgs:
+            if fArg == 'strHeader':
+                self.write( fArg[ 'strHeader' ])
+            else:
+                self._properties[ fArg ] = fArgs[ fArg ]
+
+    def write(self, strOut):
+        self._fM.write( strOut )
 
 #%% File management functions
 
 
 
 #%% Unit tests
- 
+
 if __name__ == '__main__':
-    
-    # Path tests
-    tmpLogger = clsLogger( relLoc = "path", fileName = "log-main" )
 
-    print ( tmpLogger.f.root() ) 
-    print ( tmpLogger.f.relLoc() )
-    print ( tmpLogger.f.absLoc() )
-
-    print ( tmpLogger.f.fileName() )
-    print ( tmpLogger.f.fileExt() )
-    
-    print ( tmpLogger.f.relPath() )
-    print ( tmpLogger.f.absPath() )
- 
-    
-#%% Stress tests
-
+    testBase = True
     testStress = False
 
+#%% Base tests
+
+    if testBase:
+        tmpLogger = clsLogger( fileName = "log-test", fileExt = "log" )
+
+
+
+#%% Stress tests
+
+
     if testStress:
-        print ( tmpLogger.relLoc( '\\subleft' ) )
-        print ( tmpLogger.relLoc( '\\subboth\\' ) )
-        print ( tmpLogger.relLoc( 'subright\\' ) )
-        print ( tmpLogger.relLoc( 'subnone' ) )
-        print ( tmpLogger.relLoc( 'subpar\\subchild' ) )
-        
-        print ( tmpLogger.logName( 'log-test' ) )
-        print ( tmpLogger.logName( 'log-test.txt' ) )
-        print ( tmpLogger.logExt( '.csv' ) )
-        print ( tmpLogger.logExt( 'txt' ) )
+        pass
